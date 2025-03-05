c0a020c722c7e50498d00a692453f5b1b556614d
Nguyen Van Tri <vantri1010@gmail.com>
2025-03-06 02:40:12 +0700

    Add user, product, order, and link models; implement user state management and layout components in react-admin app

--- react-admin/src/index.tsx
+++ react-admin/src/index.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from "axios";
+import {configureStore} from "./redux/configureStore";
+import {Provider} from "react-redux";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+axios.defaults.baseURL = 'http://localhost:8000/api/admin/';
+axios.defaults.withCredentials = true;
+
+const store = configureStore();
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
